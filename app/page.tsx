f2b981425f1c26007d5a142bb2ea84ac26de1cea
Aditya146699 <101649305+Aditya146699@users.noreply.github.com>
2025-01-21 17:11:04 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import KanbanBoard from "./components/KanbanBoard"
+import KanbanBoard from "./components/KanbanBoard";
 
 export default function Home() {
   return (
